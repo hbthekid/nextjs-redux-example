e3f87e7fb696009b7ecd290ab87aaf845d22553c
hbthekid <hood.ierep@gmail.com>
2019-10-26 01:04:03 -0300

    tabla

--- components/ListaTareas.js
+++ components/ListaTareas.js
@@ -1,15 +1,46 @@
 import 'antd/dist/antd.css';
+import { Table } from 'antd';
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Estado',
+        dataIndex: 'estado',
+        key: 'estado'
+    },
+    {
+        title: 'Dificultad',
+        dataIndex: 'dificultad',
+        key: 'dificultad'
+    },
+    {
+        title: 'Accion',
+        dataIndex: 'accion',
+        key: 'accion'
+    }
+  ];
+  const data = [
+
+  ]
 
 const ListaTarea = (props) => {
 
     const tareas = props.tareas.map((tarea, index) =>(
-        <div key={index}>{tarea}</div>
+        {
+            key: index,
+            name: tarea,
+            estado: 'borrachera',
+            dificultad: 'brutal',
+            accion: 'amonthat',
+        }
     ))
 
     return(
-        <div>
-            {tareas}
-        </div>
+        <Table columns={columns} dataSource={tareas} />
     )
 
 }
